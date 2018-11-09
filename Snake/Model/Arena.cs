f1bc96b34c1ef4c020a89ef6954864352880b648
Gábor Sólyom <solyomg@freemail.hu>
2018-11-09 14:15:05 +0100

    #3, #4: A játékszabályok, a játéktábla és a kígyó fejének megjelenítése

--- Snake/Model/Arena.cs
+++ Snake/Model/Arena.cs
@@ -29,7 +29,19 @@ namespace Snake.Model
             this.View = view;
             //A játék kezdetén megjelenítjük a játékszabályokat
             //Az osztályon belül a this használata nem kötelező
-            View.GamePlayTextBlock.Visibility = System.Windows.Visibility.Visible;
+            View.GamePlayBorder.Visibility = System.Windows.Visibility.Visible;
+
+            //Kígyófej megjelenítése Circle ikonnal
+            //A grid az általa tartalmazott elemeket egy gyüjteményen keresztül teszi elérhetővé
+            //ez a gyüjtemény a Children
+            //a gyüjtemény egy felsorolás, ahol az első elem a 0. indexű, a második az 1. indexű és így tovább
+            //a 10.sor 10. elemét tehát így tudjuk elkérni a gyűjteménytől
+            var cell = View.ArenaGrid.Children[10 * 20 + 10];
+            //viszont ez egy általános IUElement típust ad vissza, bármo, ami belekerül a gridbe
+            //ilyen elemként kerül bele
+            var image = (FontAwesome.WPF.ImageAwesome)cell;
+            // és már el tudom érni az ikon tulajdonságot
+            image.Icon = FontAwesome.WPF.FontAwesomeIcon.Circle;
         }
 
         internal void KeyDown(KeyEventArgs e)
@@ -42,10 +54,10 @@ namespace Snake.Model
                 case Key.Right:
                 case Key.Down:
                     //Elinditjuk a játékot: altüntetjük a játékszabályokat
-                    View.GamePlayTextBlock.Visibility = System.Windows.Visibility.Hidden;
+                    View.GamePlayBorder.Visibility = System.Windows.Visibility.Hidden;
                     View.NumberOfMealsTextBlock.Visibility = System.Windows.Visibility.Visible;
                     View.ArenaGrid.Visibility = System.Windows.Visibility.Visible;
-
+                    
                     Console.WriteLine(e.Key);
                     break;
             }
